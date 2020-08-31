0832c311c860270e70ed306342451f8fc187bebd
liaoze <616655260@qq.com>
2020-08-31 16:34:09 +0800

    optimize import

--- src/main/java/com/example/demo/task/MonitorTask.java
+++ src/main/java/com/example/demo/task/MonitorTask.java
@@ -1,28 +1,12 @@
 package com.example.demo.task;
 
-import com.alibaba.fastjson.JSON;
-import com.example.demo.controller.BpNeuralNetworkHandle;
-import com.example.demo.dao.RecordInfoMapper;
-import com.example.demo.dto.InputData;
-import com.example.demo.dto.RecordInfo;
-import com.example.demo.dto.UserInfo;
-import com.example.demo.dao.UserInfoMapper;
-import com.example.demo.mapper.ServerTableOneMapper;
 import com.example.demo.service.impl.InputService;
-import com.example.demo.util.DateUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.jni.Local;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.List;
-
-import static com.sun.tools.doclint.Entity.and;
 
 @Slf4j
 @Component
