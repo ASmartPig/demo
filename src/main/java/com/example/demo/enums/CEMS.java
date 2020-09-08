3cd85a7330590522b7c2d136249d7960c65badf0
liaoze <616655260@qq.com>
2020-09-08 13:11:47 +0800

    枚举

--- src/main/java/com/example/demo/enums/CEMS.java
+++ src/main/java/com/example/demo/enums/CEMS.java
@@ -5,16 +5,28 @@ public enum CEMS {
     CEMS_in_SO2("CEMS_in_SO2"),
     CEMS_in_flux("CEMS_in_flux"),
     CEMS_in_O2("CEMS_in_flux"),
-    CEMS_in_temp("CEMS_in_flux");
+    CEMS_in_temp("CEMS_in_flux"), Null;
 
     private String str;
 
-    public String getStr() {
-        return str;
+    CEMS() {
+    }
+
+    public static CEMS valueOfStr(String str) {
+        for (CEMS obj : CEMS.values()) {
+            if (java.util.Objects.equals(obj.str, str)) {
+                return obj;
+            }
+        }
+        return Null;
     }
 
 
     CEMS(String str) {
         this.str = str;
+    }
+
+    public String getStr() {
+        return str;
     }
 }
